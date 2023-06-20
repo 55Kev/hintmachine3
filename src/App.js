a8dfb2e83f1229b2806337d9736c88431e230b8a
Kevin <furioso_55@hotmail.com>
2023-06-20 16:13:11 +0200

    ajout aix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,109 @@ import Menu from "./components/Menu";
 import Content from "./components/Content";
 
 export default function App() {
+
 const panier = [
+  {
+    page: 6,
+    lib: "Première énigme !",
+    texteIndice: ["patronyme", "tout se nomme", "un nom de famille"]
+  },
+  {
+    page: 9,
+    lib: "A bien voir...",
+    texteIndice: ["Regardez bien", "Comparez avec le réel", "Un 'chat' au milieu de la page"]
+  },
+  {
+    page: 13,
+    lib: "Le mouton",
+    texteIndice: [
+      "Origami",
+      "Pliez vous à l'exercice",
+      "La lettre 'i' (pliez la page pour 3+6=9)"
+    ]
+  },
+  {
+    page: 16,
+    lib: "Témoignages",
+    texteIndice: [
+      "Pas de second étage",
+      'Pas de n°4 dans la rue',
+      "La lettre 'Z'"
+    ]
+  },
+  {
+    page: 18,
+    lib: "Le tournant de l'affaire",
+    texteIndice: ["Trouvez sur la façade", "Les chiffres deviennent des lettres","Windows"]
+  },
+  {
+    page: 21,
+    lib: "Les visages",
+    texteIndice: ["Pas des visages au milieu", "C'est une fontaine", "La lettre 'M', 1912 sur fontaine"]
+  },
+  {
+    page: 24,
+    lib: "Les arches",
+    texteIndice: ["Phare et Os", "Parfumeur", "Sephora"]
+  },
+  {
+    page: 26,
+    lib: "Couvre chef",
+    texteIndice: ["Façade derrière vous", "C'est presque effacé", "Chappelerie"]
+  },
+  {
+    page: 31,
+    lib: "Les croix",
+    texteIndice: ["Comptez les croix", "N'oubliez pas celle sur la fontaine", "La lettre V, 5 croix"]
+  },
+  {
+    page: 32,
+    lib: "Cardinal",
+    texteIndice: ["L'hôtel", "Bouchez les trous", "Chataignier"]
+  },
+  {
+    page: 34,
+    lib: "Botanique",
+    texteIndice: ["Fruit sculpté", "Pas l'hôtel", "Le pin"]
+  },
+  {
+    page: 36,
+    lib: "La mousse",
+    texteIndice: ["C'est rapide!", "Je double", "La lettre L, le mois prochain"]
+  },
+  {
+    page: 38,
+    lib: "Mots croisés",
+    texteIndice: ["René", "Monoprix, Durance ", "Défi"]
+  },
+  {
+    page: 41,
+    lib: "La carte",
+    texteIndice: ["Utilisez la carte", "Superposez", "Partage"]
+  },
+  {
+    page: 43,
+    lib: "La question",
+    texteIndice: ["Réponse dans le texte", "Lecture verticale", "La lettre N"]
+  },
+  {
+    page: 45,
+    lib: "La rotonde",
+    texteIndice: ["Ce n'est pas la 'Q'", "Ni la 'S'", "La lettre R"]
+  },
+  {
+    page: 47,
+    lib: "La video-surveillance",
+    texteIndice: ["Objet à trouver", "Un cadenas", "La lettre T"]
+  },
+  {
+    page: 49,
+    lib: "Dernière énigme !",
+    texteIndice: ["Vous avez la réponse en main", "1ere étape: Le stylo", "Dévissez le stylo !"]
+  }
+];
+
+const aix = [
     {
       page: 8,
       lib: "Première énigme !",
@@ -432,6 +534,14 @@ const sif = [
         ]
       }
     ];
+
+const vide = [
+    {
+      page: 0,
+      lib: "Oups!",
+      texteIndice: ["Indice1", "indice2", "Reponse"]
+    },
+  ];
 const params = new URLSearchParams(window.location.search);
 //const tabEnigmes = ((params.get("vi") === "avi") ? avi : sif);
 
@@ -440,10 +550,12 @@ const enigmeOptions = {
   "avi": avi,
   "sif": sif,
   "panier": panier,
+  "aix": aix,
+  "": vide
 };
-const tabEnigmes = (enigmeOptions.hasOwnProperty(enigmeParam)) ? enigmeOptions[enigmeParam] : "avi";
+const tabEnigmes = (enigmeOptions.hasOwnProperty(enigmeParam)) ? enigmeOptions[enigmeParam] : "vide";
 if (!tabEnigmes) {
-  tabEnigmes = enigmeOptions.avi; // Valeur par défaut si aucune option valide n'est spécifiée
+  tabEnigmes = enigmeOptions.vide; // Valeur par défaut si aucune option valide n'est spécifiée
 }
 
 const [currentPage, setCurrentPage] = useState("");
