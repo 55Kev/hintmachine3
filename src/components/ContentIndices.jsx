7f2470086ea824174ab7d21c0f538c35b689405e
55Kev <furioso_55@hotmail.com>
2024-06-21 09:57:28 +0200

    ajout fonction réponse lettre

---
+++ src/components/ContentIndices.jsx
@@ -0,0 +1,106 @@
+
+export default function ContentIndices({
+  render,
+  enigmes,
+  enigme,
+  history,
+  handleBackToMenu,
+  handleShowHint
+}) {
+  //Etats
+
+  let contenu = [];
+
+  //Comportements
+  if (!render) return;
+
+  const affEnigme = () => {
+    console.log(enigme);
+    //console.log(history);
+    for (let i = 0; i < history[enigme]; i++) {
+      if (i === enigmes[enigme].texteIndice.length - 1)
+        contenu.push("La solution :");
+      contenu.push(enigmes[enigme].texteIndice[i] + ".");
+    }
+    return contenu;
+  };
+
+  const affLib3 = () => {
+
+    if (typeof enigmes[enigme].lib === 'undefined') {
+      return (
+        <div>
+        <h3 className="text-center">page n°{enigmes[enigme].page}</h3>
+      </div>
+      );
+    } else {
+      const urlOrText = enigmes[enigme].lib;
+      
+      if (urlOrText.length > 25) {
+
+        return (
+          <img
+            className="img_full"
+            src = {urlOrText}
+            alt="page"
+          />
+        );
+      } else {
+        return (
+          <div>
+            <h3 className="text-center">page n°{enigmes[enigme].page}</h3>
+            <h4 className="text-center">{urlOrText}</h4>
+          </div>
+        );
+      }
+    }
+  };
+
+  const affLibButtonHint = () => {
+    let ret = "";
+    let classSupp = "btn btn-primary btn-lg btn-block buttonGetIndice";
+
+    if (history[enigme] === enigmes[enigme].texteIndice.length) {
+        ret = "Solution affichée";
+        classSupp = "btn btn-primary btn-lg btn-block buttonGetIndice btn-disabled"
+    } else if (history[enigme] === enigmes[enigme].texteIndice.length - 1)
+      ret = "Afficher la solution";
+    else {
+      ret =
+        "Afficher un indice " +
+        history[enigme] +
+        "/" +
+        (enigmes[enigme].texteIndice.length - 1);
+    }
+
+    return (
+      <button
+        onClick={() => handleShowHint()}
+        className={classSupp}
+         >
+         {ret}
+      </button>
+    );
+  };
+
+  //Affichage
+  return (
+    <div className="content">
+      {affLib3()}
+      <button
+        onClick={() => handleBackToMenu()}
+        className="btn btn-primary btn-lg btn-block"
+      >
+        Choisir une autre énigme
+      </button>
+      <div className="contentIndice text-center">
+        {affEnigme().map((tab, index) => (
+          <p>
+            <h3>{tab}</h3>
+          </p>
+        ))}
+      </div>
+      {affLibButtonHint()}
+    </div>
+  );
+}
---
+++ src/components/FormLetters.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import Particules from "./Particules";
+
+/*
+
+    {
+      page: 98,
+      lib_page: "Test !",
+      lib: "Vérifier la réponse à toute l'enquête - test !",
+      reponse: "Totot"
+    },
+
+    */
+
+export function FormLetters({
+    render,
+    enigmes,
+    enigme,
+    history,
+    handleBackToMenu,
+    handleShowHint
+}) {
+    
+    const [formState, setFormState] = useState(1);
+    const [reponse, setReponse] = useState('');
+    const [message, setMessage] = useState('');
+    const [affichage, setAffichage] = useState('_'.repeat(5));
+  
+    if (!render) return;
+
+    if (typeof enigmes[enigme].reponse === 'undefined') {
+        return ("Oups ! Erreur :(, la réponse n'est pas configurée");
+    } else {
+        var reponseAttendue = enigmes[enigme].reponse.toUpperCase();
+        //setAffichage('_'.repeat(reponseAttendue.length);
+    }
+
+    const ResetAndBack = () => {
+        Reset();
+        handleBackToMenu();
+    }
+
+    const Reset = () => {
+        setFormState(1);
+        setReponse('');
+        setAffichage('');
+        setMessage('');
+    }
+
+    const handleChange = (e) => {
+        const text = e.target.value;
+        setReponse(text);
+        let nouveauAffichage = '';
+        for (let i = 0; i < reponseAttendue.length; i++) {
+            setMessage('');
+            if (text.length > i) {
+            nouveauAffichage += text[i].toUpperCase();
+            } else {
+            nouveauAffichage += '_';
+            }
+        }
+        setAffichage(nouveauAffichage);
+        if ( text.toString().length === reponseAttendue.length ) {
+            if (text.toString().toUpperCase() === reponseAttendue) {
+                setMessage('Bravo ! Vous avez trouvé la bonne réponse.');
+                setFormState(2);
+            } else {
+                setMessage('Désolé, ce n\'est pas la bonne réponse. Réessayez !');
+                setReponse('');
+            }
+        }
+
+    };
+
+    const renderForm = () => {
+
+        if (typeof enigmes[enigme].lib_win_title === 'undefined') {
+            var title = "Bravo ! Félicitations !";
+        } else {
+            var title = enigmes[enigme].lib_win_title;
+        }
+
+        if (typeof enigmes[enigme].lib_win_texte === 'undefined') {
+            var texte = "Vous avez identifié le bon coupable !";
+        } else {
+            var texte = enigmes[enigme].lib_win_texte;
+        }
+       
+        if (formState === 2) {
+
+            return (
+                <div className="container text-center">
+                    <div class="row boup confettis">
+                        <h1>{title}</h1>
+                        <h4>{texte}</h4>
+                    </div>
+                    <Particules/>
+                    <button
+                        onClick={() => ResetAndBack()}
+                        className="btn btn-primary btn-lg btn-block"
+                    >
+                        Revenir aux indices
+                    </button>
+                </div>
+            );
+
+        } else {
+            return (
+                <div className="container">
+                    <form id="reponseForm">
+                        <div className='message'>{message}</div>
+                        <div className='reponse'>{affichage}</div>
+                        <div className='input'>
+                            <input
+                                type="text"
+                                value={reponse}
+                                onChange={handleChange}
+                                maxLength={reponseAttendue.length}
+                                autoCapitalize="characters"
+                                placeholder='Entrez votre réponse'
+                            />
+                        </div>
+                    </form>
+                    <button
+                        onClick={() => ResetAndBack()}
+                        className="btn btn-primary btn-lg btn-block boop"
+                    >
+                        Revenir aux indices
+                    </button>
+                </div>
+            );
+        }   
+    }
+    return renderForm();
+};
+export default FormLetters;
